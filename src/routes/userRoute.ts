089e295fe63daa16ef39b24d329164e2e98f1ffb
nsubhadipta <nsubhadipta@gmail.com>
2024-03-18 14:51:59 +0530

    add middleware in route

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,10 +1,12 @@
 import UserController from '../controllers/userController';
+import validationMiddleware from '../middlewares/validation';
+import schemas from '../middlewares/RequstValidate/user.schema';
 
 export default (router: any): void => {
 
   router.get('/users', UserController.getUsers);
   router.get('/users/:userId', UserController.getUserById);
-  router.post('/users',  UserController.createUser);
-  router.put('/users/:userId',  UserController.updateUser);
+  router.post('/users', validationMiddleware(schemas.userAdd),  UserController.createUser);
+  router.put('/users/:userId', validationMiddleware(schemas.userAdd),  UserController.updateUser);
   router.delete('/users/:userId', UserController.deleteUser);
 };
