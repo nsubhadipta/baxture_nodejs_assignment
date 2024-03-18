8c3e086f65fc45dc17bdc49f20d4be985c4e636a
Subhadipta Nayak <subhadipta.nayak@tatwa.info>
2024-03-18 14:51:40 +0530

    user schema using joi validation

---
+++ src/middlewares/RequstValidate/user.schema.ts
@@ -0,0 +1,17 @@
+import Joi from 'joi';
+
+const userSchema = {
+  userAdd: Joi.object({
+    username: Joi.string().max(50).required(),
+    age: Joi.number().integer().positive().required(),
+    hobbies: Joi.array().items(Joi.string()).default([]),
+  }),
+
+  userEdit: Joi.object({
+    username: Joi.string().max(50).required(),
+    age: Joi.number().integer().positive().required(),
+    hobbies: Joi.array().items(Joi.string()).default([]),
+  }),
+};
+
+export default userSchema;
