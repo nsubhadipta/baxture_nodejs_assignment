034cc9bcfc9dec11bad19b8651df0fa9a29ba5ce
nsubhadipta <nsubhadipta@gmail.com>
2024-03-18 14:56:30 +0530

    fixed validation issue

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -7,6 +7,6 @@ export default (router: any): void => {
   router.get('/users', UserController.getUsers);
   router.get('/users/:userId', UserController.getUserById);
   router.post('/users', validationMiddleware(schemas.userAdd),  UserController.createUser);
-  router.put('/users/:userId', validationMiddleware(schemas.userAdd),  UserController.updateUser);
+  router.put('/users/:userId', validationMiddleware(schemas.userEdit),  UserController.updateUser);
   router.delete('/users/:userId', UserController.deleteUser);
 };
