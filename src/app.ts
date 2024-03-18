a282f71c0ae44ac77f72e4c03bb42e668500d82c
nsubhadipta <nsubhadipta@gmail.com>
2024-03-18 14:27:19 +0530

    health check and 404 api

--- src/app.ts
+++ src/app.ts
@@ -8,9 +8,25 @@ dotenv.config();
 const app: Express = express();
 const router: Router = express.Router();
 
+
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 
 
+
+app.get("/api/health", async (req, res) => {
+  try {
+    res.json({ status: 1, message: "CRUD API Backend server is Running!" });
+  } catch (error) {
+    res.status(403).json({ 'error': 'error occured', message: error });
+  }
+});
+
+// Oops! The page you are looking for does not exist.
+app.get("*", function (req, res) {
+  res.status(404).send("404 Not Found: The requested URL is invalid or does not exist.");
+});
 
 const PORT = process.env.PORT || 4007;
 
